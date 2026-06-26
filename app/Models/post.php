1be918cdae1625ec84ba43c128c0c818f4de5661
Edgar Marcos <edgaricloud@gmail.com>
2026-06-26 11:24:20 +0100

    feat(model): cria model da tabela de posts

--- app/Models/post.php
+++ app/Models/post.php
@@ -9,4 +9,23 @@ class post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+
+    /**
+     * Os atributos que podem ser atribuidos em massa
+     * @var array<int,string>
+     */
+    protected $fillable = [
+        'nome',
+        'categoria',
+        'tag',
+        'conteudo',
+    ];
+
+    /**
+     * Os atributos que devem ser castrados para tipos nativos.
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'id'=>'integer',
+    ];
 }
